4df4ec18383de00b5641d8c900dcdcac8ed99f85
sara5 <sara5@DESKTOP-KB6IJF7>
2023-11-19 10:28:02 -0500

    added manufacturer to models and migrated database

--- pesticides/admin.py
+++ pesticides/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Pesticide
+from .models import Pesticide, Manufacturer
 
 admin.site.register(Pesticide)
+admin.site.register(Manufacturer)
--- pesticides/models.py
+++ pesticides/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+    
+    # implement later
+    # street1 = models.CharField(max=200)
+    # street2 = models.CharField(max=100)
+    # city = models.CharField(max_length=100)
+    # state = models.CharField(max_length=2)
+    # zip = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
 class Pesticide(models.Model):
     name = models.CharField(max_length=100)
     epa_number = models.CharField(max_length=25)
-    
+    manufacturer = models.ForeignKey(Manufacturer, null=True, blank=True, on_delete=models.SET_NULL)
     # using non-standard abbreviations for signal words for enum
     # just because I need something for the tuple
     CAUTION = "C"
@@ -31,8 +44,7 @@ class Pesticide(models.Model):
         (TRIAL, "Trial"),
         (RETIRED, "Retired"),
         (INACTIVE, "Inactive"),
-    ]
-
+    ] 
     status = models.CharField(
         max_length = 1,
         choices=STATUS_CHOICES,
