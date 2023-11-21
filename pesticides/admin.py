0fac459c7a0dec813161b27f66afa1680705d3b9
sara5 <sara5@DESKTOP-KB6IJF7>
2023-11-21 11:21:55 -0500

    added records, parks, regions to model

--- pesticides/admin.py
+++ pesticides/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Pesticide, Manufacturer, Adjuvant, Formula
-
+from .models import Pesticide, Manufacturer, Adjuvant, Formula, Region, Park, Applicator, Record
 admin.site.register(Pesticide)
 admin.site.register(Manufacturer)
 admin.site.register(Adjuvant)
 admin.site.register(Formula)
+admin.site.register(Region)
+admin.site.register(Park)
+admin.site.register(Applicator)
+admin.site.register(Record)
--- pesticides/models.py
+++ pesticides/models.py
@@ -19,7 +19,9 @@ class Adjuvant(models.Model):
     epa_number = models.CharField(max_length=25, blank=True) 
     manufacturer = models.ForeignKey(Manufacturer, null=True, on_delete=models.SET_NULL, blank=True)
     # formulas = models.ManyToManyField(Pesticide, through='Formula')
-
+    def __str__(self):
+        return self.name
+    
 class Pesticide(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     epa_number = models.CharField(max_length=25)
@@ -64,15 +66,7 @@ class Pesticide(models.Model):
         return self.name
 
 class Formula(models.Model):
-    name = models.CharField(max_length=200, blank=False, null=False)
-    pesticide = models.ForeignKey(Pesticide, on_delete=models.CASCADE)
-    adjuvant = models.ForeignKey(Adjuvant, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_length=20,
-                                 max_digits="4",
-                                 decimal_places=3,
-                                 null=True
-                                )
-    # using concentrations as choices, but could conceivably use this 
+     # using concentrations as choices, but could conceivably use this 
     # as an enum type where. I am hesistant to use enum, because 
     # python assigned the label automatically
     PERCENT_CONCENTRATION = 'P'
@@ -82,12 +76,104 @@ class Formula(models.Model):
         (PERCENT_CONCENTRATION, "%"),
         (GALLON_CONCENTATION, "/gallon")
     ]
+
+    name = models.CharField(max_length=200, blank=False, null=False)
+    pesticide = models.ForeignKey(Pesticide, on_delete=models.CASCADE)
+    adjuvant = models.ForeignKey(Adjuvant, on_delete=models.CASCADE)
+    pesticide_amount = models.DecimalField(max_length=20,
+                                 max_digits=6,
+                                 decimal_places=3,
+                                 null=True
+                                )
+    
     concentration = models.CharField(
         max_length=2, 
         choices=CONCENTRATION_CHOICES, 
         default=PERCENT_CONCENTRATION
     )
 
+    adjuvant_amount = models.DecimalField(max_length=20,
+                                 max_digits=4,
+                                 decimal_places=3,
+                                 null=True
+                                )
+   
+    adjuvant_concentration = models.CharField(
+        max_length=2, 
+        choices=CONCENTRATION_CHOICES, 
+        default=PERCENT_CONCENTRATION
+    )
 
+    def __str__(self):
+        return self.name
 
+class Region(models.Model):
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+    )
+    abbreviation = models.CharField(
+        max_length=20,
+    )
 
+    def __str__(self):
+        return self.name
+
+class Park(models.Model):
+    name = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+    )
+    abbreviation = models.CharField(
+        max_length=10,
+        null=True,
+        blank=True
+    )
+    region = models.ForeignKey(Region, models.SET_NULL, blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+class Applicator(models.Model):
+    first_name = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+    )
+    last_name = models.CharField(
+        max_length=200,
+    )
+    applicator_number = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+    )
+    home_park = models.ForeignKey(Park, models.SET_NULL, blank=True,null=True)   
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+class Record(models.Model):
+    applicator = models.ForeignKey(Applicator, models.DO_NOTHING)
+    park = models.ForeignKey(Park, models.DO_NOTHING)
+    formula = models.ForeignKey(Formula, models.DO_NOTHING)
+    area_size = models.CharField(max_length=100) 
+    date = models.DateTimeField()
+    targeted_species = models.CharField(max_length=200,null=False)
+    weather = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+    )
+
+    # expected input is 5 mph West for speed and direction 
+    wind = models.CharField
+    
+    #still need to figure out how I want to handle total amount applied
+    # due to having multiple pesticides in the formula
+    # total_amount_applied
+    def __str__(self):
+        summary = str(self.formula) + " " + str(self.park) + " " + str(self.date) 
+        return summary
