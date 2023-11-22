1aa0eadddd26266b34caa702e4559aee82f9c5ec
sara5 <sara5@DESKTOP-KB6IJF7>
2023-11-22 18:32:59 -0500

    bootstrap and table adjustments

--- pesticides/views.py
+++ pesticides/views.py
@@ -5,6 +5,7 @@ from .models import Record
 
 def index(request):
     latest_records_list = Record.objects.order_by("date")[:5]
+    print(latest_records_list)
     template = loader.get_template("pesticides/index.html")
     context = {"latest_records_list": latest_records_list}
     return HttpResponse(template.render(context, request))
