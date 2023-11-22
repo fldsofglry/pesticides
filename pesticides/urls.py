d76f033c6db3854a921479a05d24ca1a2ef1d0d9
sara5 <sara5@DESKTOP-KB6IJF7>
2023-11-22 17:54:09 -0500

    added bootstrap to project

--- pesticides/urls.py
+++ pesticides/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "pesticides"
 urlpatterns = [
     path("", views.index, name="index"),
-
+    path("<int:record_id>/", views.detail, name="detail"),
 ]
--- pesticides/views.py
+++ pesticides/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Record
 
 def index(request):
-    return HttpResponse("This is the Pesticide Index Page")
+    latest_records_list = Record.objects.order_by("date")[:5]
+    template = loader.get_template("pesticides/index.html")
+    context = {"latest_records_list": latest_records_list}
+    return HttpResponse(template.render(context, request))
+
+def detail(request, record_id):
+    return HttpResponse("You're looking at record %s" % record_id)
